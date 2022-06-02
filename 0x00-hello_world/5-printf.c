576131b3ea01ad046f4430b13bc2c421e89d75aa
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-02 07:26:29 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int main(void) /* uses puts() and returns 0*/
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
