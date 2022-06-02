7f9b685e03d384e22c1922581a795c56b64030ba
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-02 07:21:10 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
 
-
 /**
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- */
-
-/**
- * main - uses puts() function and returns 0
+ * main - prints "\"Programming is like building a multilingual puzzle"
  *
  *Return:0
  */
 int main(void) /* uses puts() and returns 0*/
 {
-	puts("\"Programming is like building a multilingual pizzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
