6679572cc05c11b8c36102f1f1a0a131cdc97098
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-02 05:52:19 -0700

    s4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (){
 
-	puts("Programming is like building a multilingual puzzle\n");
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle");    
 	return (0);
 
 }
-
-
