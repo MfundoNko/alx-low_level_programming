61f492a4854f033022b0da9a86bf42289ffe9958
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-07 17:22:15 -0700

    s7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,10 @@ int print_last_digit(int n)
 	unsigned int temp = n + (UINT_MAX + 1);
 
 	if (n == -1024)
+	{
+		_putchar(4);
 		return (4);
+	}
 	_putchar('0' + (temp % 10));
 	return (temp % 10);
 }
