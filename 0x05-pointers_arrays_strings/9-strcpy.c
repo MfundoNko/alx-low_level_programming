921928ffd2419f218c985787810aee45fc64a049
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-13 22:34:33 +0200

    s9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcpy -  copys strings
+ * @dest: string to be copied into
+ * @src: string to be copied
+ * Return: char pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return dest;
+}
