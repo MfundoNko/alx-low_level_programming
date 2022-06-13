26fdb179a5e6e1debe0fcef49986b869f950ff09
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-13 22:46:22 +0200

    s8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints elements of array
+ * @a: array to be printed
+ * @n: number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x == (n - 1))
+		{
+			printf("%d\n", a[x]);
+			continue;
+		}
+		printf("%d, ", a[x]);
+	}
+}
