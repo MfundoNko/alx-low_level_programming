301de0ac62e9b1ff9deeccf2bfe3151a4371a2ed
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-13 22:15:39 +0200

    s7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half -  prints half string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int x = strlen(str), n;
+
+	if ((x % 2) == 0)
+		n = x / 2;
+	else
+		n = (x - 1) / 2;
+	for (; n < x; n ++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
