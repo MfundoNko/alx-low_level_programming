1b8c95f4b5965780e8929fba57a18a596fdb3e23
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-16 01:14:26 -0700

    s8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -  prints size n square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
