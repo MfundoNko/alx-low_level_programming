56cecece59b3450ef5f42d01a7fcc85a942a8d0e
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-16 01:54:29 -0700

    s9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,6 +13,12 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
+		if (x == 100)
+		{
+			printf("Buzz");
+			continue;
+		}
+
 		if (((x % 3) != 0) && ((x % 5) != 0))
 		{
 			printf("%d ", x);
