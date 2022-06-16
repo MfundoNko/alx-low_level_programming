2f63c679a4e65cfba88dbe3badf28c06a3aecca5
Mfundo <mfundo.nkosi07@gmail.com>
2022-06-16 01:46:57 -0700

    s9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - prints 1-100 except for some multiples
+ * If multiple 5, prints "buzz" else if multiple 3
+ * it prints "fizz"
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (((x % 3) != 0) && ((x % 5) != 0))
+		{
+			printf("%d ", x);
+		} else
+		{
+			if (((x % 3) ==0) && ((x % 5) ==0))
+			{
+				printf("FizzBuzz ");
+			} else if ((x % 3) == 0)
+			{
+				printf("Fizz ");
+			} else if ((x % 5) == 0)
+			{
+				printf("Buzz ");
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
